2a706c531c6772ebcf20d842454a0e5fef0e510e
Atef Adel <atefadel1996123@gmail.com>
2026-02-01 12:57:59 -0800

    Create ddl_gold_items_tb.sql

---
+++ schemas/tables/ddl_gold_items_tb.sql
@@ -0,0 +1,9 @@
+IF OBJECT_ID('gold.dim_sys_item','U') IS NOT NULL
+     DROP TABLE gold.dim_sys_item
+CREATE TABLE gold.dim_sys_item (
+	itemean CHAR(13) NOT NULL PRIMARY KEY,
+	arabic_name NVARCHAR(50) NOT NULL,
+  latin_name NVARCHAR(50) NOT NULL,
+  sub_group SMALLINT NOT NULL,
+  supplier INT NOT NULL,
+)
