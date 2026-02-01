8e0066fdc29f8a54d10bba7a299f9d52dcdd64a8
Atef Adel <atefadel1996123@gmail.com>
2026-02-01 13:29:36 -0800

    Create silver.load_items.sql

---
+++ schemas/stored procedures/sys_items/silver.load_items.sql
@@ -0,0 +1,24 @@
+CREATE OR ALTER PROCEDURE silver.load_items AS
+BEGIN
+	INSERT INTO silver.erp_sys_item 
+	   (
+		itemean,
+		a_name,
+		l_name,
+		sub_group,
+		supplier 
+	    )
+	SELECT 
+	TRIM(itemean) AS itemean,
+	TRIM(a_name) AS a_name,
+	CASE
+		WHEN l_name IS NULL l_name = ''
+		THEN 'N/A'
+		ELSE l_name
+	END l_name,
+	    department AS sub_group,
+	    producerno AS supplier
+	FROM bronze.erp_sys_item ORDER BY itemean
+END
+EXEC silver.load_items
+SELECT * FROM silver.erp_sys_item 
