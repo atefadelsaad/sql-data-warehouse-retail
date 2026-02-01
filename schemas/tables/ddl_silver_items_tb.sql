67ccb621d599e70d955ec274335e0ecb0ab6dde9
Atef Adel <atefadel1996123@gmail.com>
2026-02-01 12:44:12 -0800

    Create ddl_silver_items_tb.sql for item table

---
+++ schemas/tables/ddl_silver_items_tb.sql
@@ -0,0 +1,9 @@
+IF OBJECT_ID('silver.erp_sys_item','U')IS NOT NULL
+        DROP TABLE silver.erp_sys_item
+CREATE TABLE silver.erp_sys_item(
+    itemean CHAR(13) PRIMARY KEY  NOT NULL,
+    arabic_name VARCHAR(50) NOT NULL,
+    latin_name VARCHAR(50) NOT NULL,
+    sub_group SMALLINT NOT NULL,
+    supplier INT NOT NULL
+)
