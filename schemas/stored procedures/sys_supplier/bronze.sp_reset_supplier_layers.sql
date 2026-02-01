8b5b696c3e66a598399e4cdd4ed169b434a21f4f
Atef Adel <atefadel1996123@gmail.com>
2026-02-01 14:02:38 -0800

    Rename procedure to reset supplier layers

--- schemas/stored procedures/sys_supplier/bronze.sp_reset_supplier_layers.sql
+++ schemas/stored procedures/sys_supplier/bronze.sp_reset_supplier_layers.sql
@@ -1,5 +1,4 @@
-CREATE OR ALTER PROCEDURE bronze.sp_clean_supplier
-AS
+CREATE OR ALTER PROCEDURE bronze.sp_reset_supplier_layers
 BEGIN
     TRUNCATE TABLE bronze.erp_sys_supplier
     TRUNCATE TABLE silver.erp_sys_supplier
