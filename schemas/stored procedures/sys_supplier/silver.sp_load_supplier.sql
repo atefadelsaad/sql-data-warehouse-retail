1bd3d6460c6199f9eeff36c23bc949650f607e86
Atef Adel <atefadel1996123@gmail.com>
2026-02-02 18:49:32 +0200

    Update silver.sp_load_supplier.sql

--- schemas/stored procedures/sys_supplier/silver.sp_load_supplier.sql
+++ schemas/stored procedures/sys_supplier/silver.sp_load_supplier.sql
@@ -4,8 +4,7 @@ BEGIN
 	INSERT INTO silver.erp_sys_supplier(
 		supplierno,
 		a_name,
-		l_name,
-		trademark
+		l_name
 	)
 	SELECT 
 		supplierno,
@@ -14,8 +13,7 @@ BEGIN
 			 WHEN l_name IS NULL OR l_name = '' 
 				  THEN 'N/A'
 			 ELSE TRIM(l_name)
-		END l_name,
-		COALESCE(trademark,a_name,'N/A') AS trademark
+		END l_name
 	FROM bronze.erp_sys_supplier
 END
 EXEC silver.load_supplier
