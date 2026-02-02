d13fe7fe6bf6f56faebec03716137b4c2de0981a
Atef Adel <atefadel1996123@gmail.com>
2026-02-02 21:51:29 +0200

    Create ddl_gold_doctype_tb.sql

---
+++ schemas/tables/ddl_gold_doctype_tb.sql
@@ -0,0 +1,14 @@
+
+IF OBJECT_ID('gold.dim_sys_doctype','U')IS NOT NULL
+     DROP TABLE gold.dim_sys_doctypee
+CREATE TABLE gold.dim_sys_doctype(
+  [systemcode] [smallint] NOT NULL,
+	[transtype] [smallint] NOT NULL,
+	[doctype] [smallint] NOT NULL,
+	[a_name] [varchar](50) NOT NULL,
+	[l_name] [varchar](50) NOT NULL,
+  CONSTRAINT [pk_sys_doctype] PRIMARY KEY NONCLUSTERED 
+  ([systemcode] ASC,
+	[transtype] ASC,
+	[doctype] ASC)
+)
