ef468d92c003345722495e8e9e496539df9eed55
Atef Adel <atefadel1996123@gmail.com>
2026-02-02 20:15:01 +0200

    Update bronze.sp_reset_items_layers.sql

--- schemas/stored procedures/sys_items/bronze.sp_reset_items_layers.sql
+++ schemas/stored procedures/sys_items/bronze.sp_reset_items_layers.sql
@@ -1,6 +1,6 @@
-CREATE PROCEDURE bronze.sp_reset_items_layers 
+CREATE PROCEDURE bronze.reset_items_layers 
 AS
 BEGIN
 	TRUNCATE TABLE bronze.erp_sys_item
-  TRUNCATE TABLE silver.erp_sys_item
+    TRUNCATE TABLE silver.erp_sys_item
 END
