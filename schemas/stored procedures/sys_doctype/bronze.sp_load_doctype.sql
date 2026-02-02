0ec840bc31d6be6ba19f6f4490c01fa909caf30b
Atef Adel <atefadel1996123@gmail.com>
2026-02-02 21:26:00 +0200

    Create bronze.sp_load_doctype.sql

---
+++ schemas/stored procedures/sys_doctype/bronze.sp_load_doctype.sql
@@ -0,0 +1,43 @@
+CREATE OR ALTER PROCEDURE bronze.load_doctype
+AS
+BEGIN
+INSERT INTO db_Atef.bronze.erp_sys_doctype(
+		 [systemcode]
+		,[transtype]
+		,[doctype]
+		,[a_name]
+		,[l_name]
+		,[document]
+		,[site_serial]
+		,[manual_serial]
+		,[period]
+		,[program_name]
+		,[program_type]
+		,[nature]
+		,[doc_group]
+		,[usage]
+		,[transdate]
+		,[status]
+		,[currency]
+      )
+SELECT
+		 [systemcode]
+		,[transtype]
+		,[doctype]
+		,[a_name]
+		,[l_name]
+		,[document]
+		,[site_serial]
+		,[manual_serial]
+		,[period]
+		,[program_name]
+		,[program_type]
+		,[nature]
+		,[doc_group]
+		,[usage]
+		,[transdate]
+		,[status]
+		,[currency]
+FROM Retail.dbo.sys_doctype
+END
+EXEC bronze.load_doctype
