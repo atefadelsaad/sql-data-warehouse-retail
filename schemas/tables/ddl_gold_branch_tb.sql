628f9023844a5bfb13a6947eaeea78759d39e73d
Atef Adel <atefadel1996123@gmail.com>
2026-02-02 20:25:25 +0200

    Create ddl_gold_branch_tb.sql

---
+++ schemas/tables/ddl_gold_branch_tb.sql
@@ -0,0 +1,15 @@
+IF OBJECT_ID('gold.dim_branch','U') IS NOT NULL
+      DROP TABLE gold.dim_branch
+CREATE TABLE gold.dim_branch(
+	[company] [smallint] NOT NULL,
+	[sector] [smallint] NOT NULL,
+	[region] [smallint] NOT NULL,
+	[branch] [int] NOT NULL,
+	[branchtype] [smallint] NOT NULL,
+	[activity] [smallint] NOT NULL,
+	[a_name] [char](50) NOT NULL,
+	[l_name] [char](50) NOT NULL,
+	[country] [smallint] NOT NULL,
+CONSTRAINT [pk_erp_sys_branch] PRIMARY KEY NONCLUSTERED ([branch] ASC)
+)
+
