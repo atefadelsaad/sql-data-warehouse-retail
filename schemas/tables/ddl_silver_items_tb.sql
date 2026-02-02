54a214063bd69af343d85bb44cb1b25a688a22ad
Atef Adel <atefadel1996123@gmail.com>
2026-02-02 19:47:07 +0200

    Update ddl_silver_items_tb.sql

--- schemas/tables/ddl_silver_items_tb.sql
+++ schemas/tables/ddl_silver_items_tb.sql
@@ -1,7 +1,7 @@
 IF OBJECT_ID('silver.erp_sys_item','U')IS NOT NULL
         DROP TABLE silver.erp_sys_item
 CREATE TABLE silver.erp_sys_item(
-    itemean CHAR(13) PRIMARY KEY  NOT NULL,
+    itemean CHAR(13)  NOT NULL PRIMARY KEY ,
     arabic_name VARCHAR(50) NOT NULL,
     latin_name VARCHAR(50) NOT NULL,
     sub_group SMALLINT NOT NULL,
