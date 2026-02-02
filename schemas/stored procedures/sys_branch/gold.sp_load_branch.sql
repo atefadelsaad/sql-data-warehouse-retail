d2fba6f0ced224105eebc815fb441b82f1fe8b8e
Atef Adel <atefadel1996123@gmail.com>
2026-02-02 20:33:55 +0200

    Create gold.sp_load_branch.sql

---
+++ schemas/stored procedures/sys_branch/gold.sp_load_branch.sql
@@ -0,0 +1,29 @@
+CREATE OR ALTER PROCEDURE gold.load_branch
+AS
+BEGIN
+INSERT INTO gold.dim_branch
+     (
+	 [company]
+	,[sector]
+	,[region]
+	,[branch]
+	,[branchtype]
+	,[activity]
+	,[a_name]
+	,[l_name]
+	,[country]
+     )
+SELECT
+  [company]
+  ,[sector]
+  ,[region]
+  ,[branch]
+  ,[branchtype]
+  ,[activity]
+  ,[a_name]
+  ,[l_name]
+  ,[country]
+FROM [silver].[erp_sys_branch]
+END
+
+EXEC gold.load_branch
