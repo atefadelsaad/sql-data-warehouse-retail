f9423d964d4951a6bb4b5ae00569ceb00ddff8dd
Atef Adel <atefadel1996123@gmail.com>
2026-02-02 22:03:26 +0200

    Update ddl_gold_items_tb.sql

--- schemas/tables/ddl_gold_items_tb.sql
+++ schemas/tables/ddl_gold_items_tb.sql
@@ -1,8 +1,8 @@
-IF OBJECT_ID('gold.dim_sys_item','U') IS NOT NULL
-     DROP TABLE gold.dim_sys_item
-CREATE TABLE gold.dim_sys_item (
-	itemean CHAR(13) NOT NULL PRIMARY KEY,
-	arabic_name NVARCHAR(50) NOT NULL,
+IF OBJECT_ID('gold.dim_item','U') IS NOT NULL
+     DROP TABLE gold.dim_item
+CREATE TABLE gold.dim_item (
+  itemean CHAR(13) NOT NULL PRIMARY KEY,
+  arabic_name NVARCHAR(50) NOT NULL,
   latin_name NVARCHAR(50) NOT NULL,
   sub_group SMALLINT NOT NULL,
   supplier INT NOT NULL,
