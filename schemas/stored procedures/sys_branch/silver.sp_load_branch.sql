bfdf3af1313b8d80755b458f5e6b43f399328368
Atef Adel <atefadel1996123@gmail.com>
2026-02-03 12:09:29 -0800

    Update silver.sp_load_branch.sql

--- schemas/stored procedures/sys_branch/silver.sp_load_branch.sql
+++ schemas/stored procedures/sys_branch/silver.sp_load_branch.sql
@@ -1,29 +1,28 @@
-CREATE OR ALTER PROCEDURE silver.load_branch 
+CREATE OR ALTER PROCEDURE silver.load_branch
 AS
 BEGIN
+    INSERT INTO silver.erp_sys_branch (
+        company,
+        sector,
+        region,
+        branch,
+        branchtype,
+        activity,
+        a_name,
+        l_name,
+        country
+    )
+    SELECT 
+        company,
+        sector,
+        region,
+        branch,
+        branchtype,
+        ISNULL(activity, 0) AS activity,
+        TRIM(a_name) AS a_name,
+        TRIM(l_name) AS l_name,
+        ISNULL(country, 0) AS country
+    FROM bronze.erp_sys_branch;
+END;
 
-	INSERT INTO silver.erp_sys_branch
-	  )
-		 [company]
-		,[sector]
-		,[region]
-		,[branch]
-		,[branchtype]
-		,[activity]
-		,[a_name]
-		,[l_name]
-		,[country]
-	  )
-	SELECT 
-		company,
-		sector,
-		region,
-		branch,
-		branchtype,
-		ISNULL(activity,0) AS activity,
-		TRIM(a_name) as a_name,
-		TRIM(l_name) as l_name,
-		ISNULL(country,0) AS country
-	FROM bronze.erp_sys_branch
-END
-EXEC silver.load_branch 
+EXEC silver.load_branch;
