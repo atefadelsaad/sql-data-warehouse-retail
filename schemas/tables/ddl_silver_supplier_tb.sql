35af3ab43c79b4c065487962c57c88a7f84dd365
Atef Adel <atefadel1996123@gmail.com>
2026-02-03 12:06:50 -0800

    Update ddl_silver_supplier_tb.sql

--- schemas/tables/ddl_silver_supplier_tb.sql
+++ schemas/tables/ddl_silver_supplier_tb.sql
@@ -1,8 +1,9 @@
 IF OBJECT_ID('silver.erp_sys_supplier','U') IS NOT NULL
-     DROP TABLE silver.erp_sys_supplier
+    DROP TABLE silver.erp_sys_supplier;
+
 CREATE TABLE silver.erp_sys_supplier(
-	[supplierno] [int] NOT NULL ,
-	[a_name] [varchar](50) NOT NULL,
-	[l_name] [varchar](50) NOT NULL,
-	CONSTRAINT [pk_erp_sys_supplier] PRIMARY KEY NONCLUSTERED ([supplierno] ASC)
-)
+    supplierno int NOT NULL,
+    a_name varchar(50) NOT NULL,
+    l_name varchar(50) NOT NULL,
+    CONSTRAINT pk_erp_sys_supplier PRIMARY KEY NONCLUSTERED (supplierno ASC)
+);
