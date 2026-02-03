1a8e30f63999cbef6657d086ceca5f75f3265741
Atef Adel <atefadel1996123@gmail.com>
2026-02-03 12:11:10 -0800

    Update gold.sp_load_documents.sql

--- schemas/stored procedures/sys_doctype/gold.sp_load_documents.sql
+++ schemas/stored procedures/sys_doctype/gold.sp_load_documents.sql
@@ -1,20 +1,20 @@
 CREATE OR ALTER PROCEDURE gold.load_documents
 AS
 BEGIN
-	INSERT INTO gold.dim_documents_types
-		(
-		 [systemcode]
-		,[transtype]
-		,[doctype]
-		,[a_name]
-		,[l_name]
-		)
-	SELECT 
-		 [systemcode]
-		,[transtype]
-		,[doctype]
-		,[a_name]
-		,[l_name]
-	  FROM silver.erp_sys_doctype
-END
-EXEC gold.load_documents
+    INSERT INTO gold.dim_documents_types (
+        systemcode,
+        transtype,
+        doctype,
+        a_name,
+        l_name
+    )
+    SELECT 
+        systemcode,
+        transtype,
+        doctype,
+        a_name,
+        l_name
+    FROM silver.erp_sys_doctype;
+END;
+
+EXEC gold.load_documents;
