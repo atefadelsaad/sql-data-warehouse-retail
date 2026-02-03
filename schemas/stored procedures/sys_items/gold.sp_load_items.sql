7052abe036bf61a8345b52a54709575287c00a15
Atef Adel <atefadel1996123@gmail.com>
2026-02-03 12:14:16 -0800

    Update gold.sp_load_items.sql

--- schemas/stored procedures/sys_items/gold.sp_load_items.sql
+++ schemas/stored procedures/sys_items/gold.sp_load_items.sql
@@ -1,21 +1,22 @@
-CREATE OR ALTER PROCEDURE gold.load_items AS
+CREATE OR ALTER PROCEDURE gold.load_items
+AS
 BEGIN
-	INSERT INTO gold.dim_item
-	   (
-		itemean,
-		arabic_name,
-		latin_name,
-		sub_group,
-		supplier 
-	    )
-     SELECT
-		itemean,
-		arabic_name,
-		latin_name,
-		sub_group,
-		supplier 
-     FROM silver.erp_sys_item
-END
-
-EXEC gold.load_items
-SELECT * from gold.dim_sys_item
+    INSERT INTO gold.dim_item (
+        itemean,
+        arabic_name,
+        latin_name,
+        sub_group,
+        supplier
+    )
+    SELECT
+        itemean,
+        arabic_name,
+        latin_name,
+        sub_group,
+        supplier
+    FROM silver.erp_sys_item;
+END;
+  
+EXEC gold.load_items;
+  
+SELECT * FROM gold.dim_item;
