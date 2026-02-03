d91e325d49360c6ae8fbc0ba06366caad4d6c285
Atef Adel <atefadel1996123@gmail.com>
2026-02-03 12:08:51 -0800

    Update gold.sp_load_branch.sql

--- schemas/stored procedures/sys_branch/gold.sp_load_branch.sql
+++ schemas/stored procedures/sys_branch/gold.sp_load_branch.sql
@@ -1,29 +1,28 @@
 CREATE OR ALTER PROCEDURE gold.load_branch
 AS
 BEGIN
-INSERT INTO gold.dim_branch
-     (
-	 [company]
-	,[sector]
-	,[region]
-	,[branch]
-	,[branchtype]
-	,[activity]
-	,[a_name]
-	,[l_name]
-	,[country]
-     )
-SELECT
-  [company]
-  ,[sector]
-  ,[region]
-  ,[branch]
-  ,[branchtype]
-  ,[activity]
-  ,[a_name]
-  ,[l_name]
-  ,[country]
-FROM [silver].[erp_sys_branch]
-END
+    INSERT INTO gold.dim_branch (
+        company,
+        sector,
+        region,
+        branch,
+        branchtype,
+        activity,
+        a_name,
+        l_name,
+        country
+    )
+    SELECT
+        company,
+        sector,
+        region,
+        branch,
+        branchtype,
+        activity,
+        a_name,
+        l_name,
+        country
+    FROM silver.erp_sys_branch;
+END;
 
-EXEC gold.load_branch
+EXEC gold.load_branch;
