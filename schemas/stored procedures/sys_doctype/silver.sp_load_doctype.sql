dad31c9452ff7c8b6578ebb09578f4140d1798af
Atef Adel <atefadel1996123@gmail.com>
2026-02-03 12:11:59 -0800

    Update silver.sp_load_doctype.sql

--- schemas/stored procedures/sys_doctype/silver.sp_load_doctype.sql
+++ schemas/stored procedures/sys_doctype/silver.sp_load_doctype.sql
@@ -1,24 +1,23 @@
 CREATE OR ALTER PROCEDURE silver.load_doctype
 AS
 BEGIN
-	INSERT INTO silver.erp_sys_doctype
-			   (
-				   systemcode
-			     transtype
-			     doctype
-			     a_name
-			     l_name
-			   ) 
-	SELECT 
-	      	systemcode,
-	       	transtype,
-      	 	doctype,
-       		a_name,
-      		CASE 
-		          	WHEN l_name IS NULL OR l_name = ''
-		          		THEN 'N/A'
-		          	ELSE l_name
-	      	END l_name
-	FROM bronze.erp_sys_doctype 
-END
-EXEC silver.load_doctype
+    INSERT INTO silver.erp_sys_doctype (
+        systemcode,
+        transtype,
+        doctype,
+        a_name,
+        l_name
+    )
+    SELECT 
+        systemcode,
+        transtype,
+        doctype,
+        a_name,
+        CASE 
+            WHEN l_name IS NULL OR l_name = '' THEN 'N/A'
+            ELSE l_name
+        END AS l_name
+    FROM bronze.erp_sys_doctype;
+END;
+
+EXEC silver.load_doctype;
