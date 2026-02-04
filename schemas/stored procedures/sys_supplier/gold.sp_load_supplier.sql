a93dee6f143fbad211e6656c5efe6e0a435243f9
Atef Adel <atefadel1996123@gmail.com>
2026-02-04 18:51:54 +0200

    Update gold.sp_load_supplier.sql

--- schemas/stored procedures/sys_supplier/gold.sp_load_supplier.sql
+++ schemas/stored procedures/sys_supplier/gold.sp_load_supplier.sql
@@ -1,17 +1,33 @@
+
 CREATE OR ALTER PROCEDURE gold.load_supplier
 AS
 BEGIN
-	INSERT INTO gold.dim_supplier
-	   (
-    	  supplierno,
-	     	a_name,
-	    	l_name
-	    )
-  SELECT
-	     supplierno,
-		   a_name,
-		   l_name
-     FROM silver.erp_sys_supplier
-END
+    SET NOCOUNT ON;
 
-	
+    MERGE gold.dim_supplierAS target
+    USING silver.erp_sys_supplier   AS source
+        ON target.itemean = source.itemean
+
+    WHEN MATCHED
+                AND 
+                   target.arabic_name<> source.arabic_name
+                OR target.latin_name <> source.latin_name
+                OR target.sub_group <> source.sub_group
+                OR target.supplier <> source.supplier
+    THEN
+        UPDATE SET
+            target.arabic_name = source.arabic_name,
+            target.latin_name = source.latin_name,
+            target.sub_group = source.sub_group,
+            target.supplier = source.supplier
+
+    WHEN NOT MATCHED BY TARGET
+    THEN
+        INSERT (itemean,arabic_name, latin_name,sub_group,supplier)
+        VALUES (source.itemean,source.arabic_name,source.latin_name,source.sub_group,source.supplier)
+
+    WHEN NOT MATCHED BY SOURCE
+    THEN
+        DELETE;
+
+END;
