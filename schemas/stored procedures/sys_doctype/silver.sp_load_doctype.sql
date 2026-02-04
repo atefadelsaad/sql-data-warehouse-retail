a43f9f4ea231f3f38a473b4ead441a2e3bd113ba
Atef Adel <atefadel1996123@gmail.com>
2026-02-04 19:32:45 +0200

    Update silver.sp_load_doctype.sql

--- schemas/stored procedures/sys_doctype/silver.sp_load_doctype.sql
+++ schemas/stored procedures/sys_doctype/silver.sp_load_doctype.sql
@@ -1,23 +1,45 @@
 CREATE OR ALTER PROCEDURE silver.load_doctype
 AS
 BEGIN
-    INSERT INTO silver.erp_sys_doctype (
-        systemcode,
-        transtype,
-        doctype,
-        a_name,
-        l_name
-    )
-    SELECT 
-        systemcode,
-        transtype,
-        doctype,
-        a_name,
-        CASE 
-            WHEN l_name IS NULL OR l_name = '' THEN 'N/A'
-            ELSE l_name
-        END AS l_name
-    FROM bronze.erp_sys_doctype;
-END;
+INSERT INTO silver.erp_sys_doctype (
+    systemcode,
+    transtype,
+    doctype,
+    a_name,
+    l_name
+)
+SELECT 
+    systemcode,
+    transtype,
+    doctype,
+    a_name,
+    CASE 
+        WHEN l_name IS NULL OR l_name = '' THEN 'N/A'
+        ELSE l_name
+    END AS l_name
+FROM bronze.erp_sys_doctype
+WHERE
+		systemcode IN (10)
 
-EXEC silver.load_doctype;
+
+INSERT INTO silver.erp_sys_doctype (
+    systemcode,
+    transtype,
+    doctype,
+    a_name,
+    l_name
+)
+SELECT 
+    systemcode,
+    transtype,
+    doctype,
+    a_name,
+    CASE 
+        WHEN l_name IS NULL OR l_name = '' THEN 'N/A'
+        ELSE l_name
+    END AS l_name
+FROM bronze.erp_sys_doctype
+		WHERE
+			systemcode IN (2) AND usage = 1 -- stock
+			AND doctype NOT IN (SELECT doctype FROM silver.erp_sys_doctype)
+END;
