0148a8a4be5b13e7b5415260663fc6d96e83fa5a
Atef Adel <atefadel1996123@gmail.com>
2026-02-04 18:39:16 +0200

    Update gold.sp_load_branch.sql

--- schemas/stored procedures/sys_branch/gold.sp_load_branch.sql
+++ schemas/stored procedures/sys_branch/gold.sp_load_branch.sql
@@ -1,28 +1,41 @@
-CREATE OR ALTER PROCEDURE gold.load_branch
+CREATE OR ALTER PROCEDURE gold.update_branch
 AS
 BEGIN
-    INSERT INTO gold.dim_branch (
-        company,
-        sector,
-        region,
-        branch,
-        branchtype,
-        activity,
-        a_name,
-        l_name,
-        country
-    )
-    SELECT
-        company,
-        sector,
-        region,
-        branch,
-        branchtype,
-        activity,
-        a_name,
-        l_name,
-        country
-    FROM silver.erp_sys_branch;
-END;
+    SET NOCOUNT ON;
 
-EXEC gold.load_branch;
+    MERGE gold.dim_branch AS target
+    USING silver.erp_sys_branch AS source
+        ON target.branch = source.branch
+
+    WHEN MATCHED
+               AND 
+                    target.company <> source.company
+                 or target.sector <> source.sector
+                 or target.region <> source.region
+                 or target.branchtype <> source.branchtype
+                 or target.activity  <> source.activity
+                 or target.a_name <> source.a_name
+                 or target.l_name  <> source.l_name
+                 or target.country <> source.country
+    THEN
+        UPDATE SET
+            target.company = source.company,
+            target.sector = source.sector,
+            target.region = source.region,
+            target.branchtype = source.branchtype,
+            target.activity = source.activity,
+            target.a_name = source.a_name,
+            target.l_name = source.l_name,
+            target.country = source.country
+        
+    WHEN NOT MATCHED BY TARGET
+    THEN
+        INSERT (company, sector,region,branch,branchtype,activity,a_name,l_name,country)
+        VALUES (source.company, source.sector,source.region,source.branch,source.branchtype,source.activity,source.a_name,source.l_name,source.country)
+
+    WHEN NOT MATCHED BY SOURCE
+    THEN
+        DELETE;
+
+END;
+GO
