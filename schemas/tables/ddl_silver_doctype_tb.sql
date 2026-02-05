22a47d3e66ece814117792201eb3c25847012eaa
Atef Adel <atefadel1996123@gmail.com>
2026-02-05 13:21:49 +0200

    Update ddl_silver_doctype_tb.sql

--- schemas/tables/ddl_silver_doctype_tb.sql
+++ schemas/tables/ddl_silver_doctype_tb.sql
@@ -2,8 +2,6 @@ IF OBJECT_ID('silver.erp_sys_doctype','U') IS NOT NULL
     DROP TABLE silver.erp_sys_doctype;
 
 CREATE TABLE silver.erp_sys_doctype(
-    systemcode smallint NOT NULL,
-    transtype smallint NOT NULL,
     doctype smallint NOT NULL,
     a_name varchar(50) NOT NULL,
     l_name varchar(50) NOT NULL,
