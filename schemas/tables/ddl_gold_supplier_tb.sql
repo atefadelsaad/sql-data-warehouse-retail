e29d33fc7d24718101d5c9dd892526609dd1c53d
Atef Adel <atefadel1996123@gmail.com>
2026-02-05 15:18:16 +0200

    Update ddl_gold_supplier_tb.sql

--- schemas/tables/ddl_gold_supplier_tb.sql
+++ schemas/tables/ddl_gold_supplier_tb.sql
@@ -3,7 +3,7 @@ IF OBJECT_ID('gold.dim_supplier','U') IS NOT NULL
 
 CREATE TABLE gold.dim_supplier(
     supplierno int NOT NULL PRIMARY KEY,
-    a_name varchar(50) NOT NULL,
-    l_name varchar(50) NOT NULL,
+    arabic_name varchar(50) NOT NULL,
+    latin_name varchar(50) NOT NULL,
     last_update datetime NOT NULL
 );
