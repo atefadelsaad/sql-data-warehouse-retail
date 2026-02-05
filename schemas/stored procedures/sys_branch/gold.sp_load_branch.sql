e5e8a51ca080938890e6e63c8cab4e3a96fd43bd
Atef Adel <atefadel1996123@gmail.com>
2026-02-05 15:29:32 +0200

    Update gold.sp_load_branch.sql

--- schemas/stored procedures/sys_branch/gold.sp_load_branch.sql
+++ schemas/stored procedures/sys_branch/gold.sp_load_branch.sql
@@ -10,20 +10,20 @@ BEGIN
 
     WHEN MATCHED
                AND (
-		          target.arabic_name <> source.a_name
-                  or target.latin_name  <> source.l_name
-                  or target.branch_type <> source.branchtype
+		          target.arabic_name <> source.arabic_name
+                  or target.latin_name  <> source.latin_name
+                  or target.branch_type <> source.branch_type
                    )
     THEN
         UPDATE SET
-            target.arabic_name = source.a_name,
-            target.latin_name = source.l_name,
-            target.branch_type = source.branchtype,
+            target.arabic_name = source.arabic_name,
+            target.latin_name = source.latin_name,
+            target.branch_type = source.branch_type,
             target.last_update = getdate()
     WHEN NOT MATCHED BY TARGET
     THEN
         INSERT (branch,arabic_name,latin_name,branch_type,last_update)
-        VALUES (source.branch, source.a_name, source.l_name, source.branchtype, GETDATE())
+        VALUES (source.branch, source.arabic_name, source.latin_name, source.branch_type, GETDATE())
     WHEN NOT MATCHED BY SOURCE
     THEN
         DELETE
