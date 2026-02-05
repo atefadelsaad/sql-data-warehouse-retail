2dd660bb171a93b1dc8b243ecc0faafbf650c410
Atef Adel <atefadel1996123@gmail.com>
2026-02-05 12:48:24 +0200

    Update ddl_silver_branch_tb.sql

--- schemas/tables/ddl_silver_branch_tb.sql
+++ schemas/tables/ddl_silver_branch_tb.sql
@@ -2,14 +2,9 @@ IF OBJECT_ID('silver.erp_sys_branch','U') IS NOT NULL
     DROP TABLE silver.erp_sys_branch;
 
 CREATE TABLE silver.erp_sys_branch(
-    company smallint NOT NULL,
-    sector smallint NOT NULL,
-    region smallint NOT NULL,
     branch int NOT NULL,
     branchtype smallint NOT NULL,
-    activity smallint NOT NULL,
     a_name char(50) NOT NULL,
     l_name char(50) NOT NULL,
-    country smallint NOT NULL,
     CONSTRAINT pk_erp_sys_branch PRIMARY KEY NONCLUSTERED (branch ASC)
 );
