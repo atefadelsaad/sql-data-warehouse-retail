dfbdd0ab2c354abe173ac52bd40794088596a323
Atef Adel <atefadel1996123@gmail.com>
2026-02-05 13:23:29 +0200

    Update ddl_gold_documents_tb.sql

--- schemas/tables/ddl_gold_documents_tb.sql
+++ schemas/tables/ddl_gold_documents_tb.sql
@@ -2,11 +2,9 @@ IF OBJECT_ID('gold.dim_documents_types','U') IS NOT NULL
     DROP TABLE gold.dim_documents_types;
 
 CREATE TABLE gold.dim_documents_types(
-    systemcode smallint NOT NULL,
-    transtype smallint NOT NULL,
     doctype smallint NOT NULL,
-    a_name varchar(50) NOT NULL,
-    l_name varchar(50) NOT NULL,
+    arabic)name varchar(50) NOT NULL,
+    latin_name varchar(50) NOT NULL,
     last_update datetime NOT NULL,
     CONSTRAINT pk_sys_doctype PRIMARY KEY NONCLUSTERED (
         doctype ASC
