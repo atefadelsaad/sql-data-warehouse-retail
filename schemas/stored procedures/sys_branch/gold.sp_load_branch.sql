9d65c33e9fa2de9a83a193a243624acd8f502272
Atef Adel <atefadel1996123@gmail.com>
2026-02-05 12:46:51 +0200

    Update gold.sp_load_branch.sql

--- schemas/stored procedures/sys_branch/gold.sp_load_branch.sql
+++ schemas/stored procedures/sys_branch/gold.sp_load_branch.sql
@@ -10,29 +10,19 @@ BEGIN
 
     WHEN MATCHED
                AND 
-                    target.company <> source.company
-                 or target.sector <> source.sector
-                 or target.region <> source.region
-                 or target.branchtype <> source.branchtype
-                 or target.activity  <> source.activity
+                    target.branchtype <> source.branchtype
                  or target.a_name <> source.a_name
                  or target.l_name  <> source.l_name
-                 or target.country <> source.country
     THEN
         UPDATE SET
-            target.company = source.company,
-            target.sector = source.sector,
-            target.region = source.region,
             target.branchtype = source.branchtype,
-            target.activity = source.activity,
             target.a_name = source.a_name,
             target.l_name = source.l_name,
-            target.country = source.country
         
     WHEN NOT MATCHED BY TARGET
     THEN
-        INSERT (company, sector,region,branch,branchtype,activity,a_name,l_name,country,last_update)
-        VALUES (source.company, source.sector,source.region,source.branch,source.branchtype,source.activity,source.a_name,source.l_name,source.country,getdate())
+        INSERT (branch,branchtype,a_name,l_name,last_update)
+        VALUES (source.branch,source.branchtype,source.a_name,source.l_name,getdate())
     WHEN NOT MATCHED BY SOURCE
     THEN
         DELETE
